82585d739c878496be778dc66fa74f81275338b9
Flavio Santos <flawyo2002@gmail.com>
2025-05-21 23:20:09 -0300

    feat: adiciona leitura da lista de adjacência

---
+++ Conexidade de Grafos/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include "Grafo.hpp"
+
+using namespace std;
+
+int main(){
+
+    int n; //nº de vertices
+    int m; //nº de arestas
+    int u, v;
+    cin >> n >> m;
+
+    Grafo grafo(n, m);
+
+    for(int i = 0; i < m; i++){
+        cin >> u >> v;
+        grafo.adicionaAresta(u, v);
+    }
+
+    grafo.imprimiListaAdj();
+
+    return 0;
+}
